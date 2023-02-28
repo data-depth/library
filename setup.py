fc0807d19df3089cda0954d5ac05e55f0d636611
Romain Valla <95872435+romainvalla06@users.noreply.github.com>
2023-02-28 14:50:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ class custom_build_ext(build_ext):
         # removes the "default" compiler flags that would
         # otherwise get passed on to to the compiler, i.e.,
         # distutils.sysconfig.get_var("CFLAGS").
-        self.compiler.set_executable("compiler_so", "g++ -fpic -O2")
+        self.compiler.set_executable("compiler_so", "g++ -fpic")
         self.compiler.set_executable("compiler_cxx", "g++")
         if sys.platform=='darwin':
         	self.compiler.set_executable("linker_so", "g++ -Wl, -shared -lstdc++")
@@ -34,12 +34,12 @@ if sys.platform=='darwin' or sys.platform=='linux':
 		    "ddalpha", 
 		    sources=["depth/src/ddalpha.cpp"],
 		    extra_compile_args=["-I.",'-std=c++14','-fPIC','-O2'],
-		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC','-O2']
+		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
 		),Extension(
 		    "depth_wrapper", 
 		    sources=["depth/src/depth_wrapper.cpp"],
 		    extra_compile_args=["-I.",'-std=c++14','-fPIC','-O2'],
-		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC','-O2']
+		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
 		)
 	     
 	   	
