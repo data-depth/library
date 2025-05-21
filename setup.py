3a5006671868962cb20f24447cf56051552db294
LEONE <43539948+leoneleo@users.noreply.github.com>
2025-05-21 14:47:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ class custom_build_ext(build_ext):
 if sys.platform=='darwin' or sys.platform=='linux':
 	setup(
 	    name="data_depth",
-	    version="1.0.1",
+	    version="1.1.0",
 	    author="Pavlo Mozharovskyi",
 	    author_email="pavlo.mozharovskyi@telecom-paris.fr",
 	    description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
@@ -51,7 +51,7 @@ if sys.platform=='darwin' or sys.platform=='linux':
 if sys.platform=='win32':
 	setup(
 	    name="data_depth",
-	    version="1.0.1",
+	    version="1.1.0",
 	    author="Pavlo Mozharovskyi",
 	    author_email="pavlo.mozharovskyi@telecom-paris.fr",
 	    description="The package provides many procedures for calculating the depth of points in an empirical distribution for many notions of data depth",
