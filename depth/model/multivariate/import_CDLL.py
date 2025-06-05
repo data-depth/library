1c73c41523eab2c2554ae1ac24e4b46e50a00067
Leonardo Leone <leonardo.leone1998@gmail.com>
2025-06-05 15:56:17 +0200

    fix site_packages

--- depth/model/multivariate/import_CDLL.py
+++ depth/model/multivariate/import_CDLL.py
@@ -27,7 +27,6 @@ def import_CDLL():
 
     if sys.platform=='win32':
         site_packages = [p for p in sys.path if ('site-packages' in p) or ("dist-packages" in p)] #Add search dist-packages 
-        site_packages.append("C:\\Users\\lleone\\Desktop\\DEPTHLIBRARY\\library\\")
         for i in site_packages:
             dll_path = os.path.join(i, 'depth', 'src')
             if os.path.isdir(dll_path): #check it is a real dir
