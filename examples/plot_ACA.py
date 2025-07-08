4a4785b19328f5a6009a0e35c6a017e68e0e4460
Pavlo Mozharovskyi <pavlo.mozharovskyi@telecom-paris.fr>
2025-07-08 11:31:17 +0100

    Fix of ACA, to put online.

--- examples/plot_ACA.py
+++ examples/plot_ACA.py
@@ -17,9 +17,11 @@ from matplotlib import pyplot as plt
 np.random.seed(2801)
 mat1=[[1, 0, 0, 0, 0],[0, 1, 0, 0, 0],[0, 0, 1, 0, 0],[0, 0, 0, 1, 0],[0, 0, 0, 0, 1]]
 mat2=[[0.25, 0, 0, 0, 0],[0, 0.25, 0, 0, 0],[0, 0, 0.25, 0, 0],[0, 0, 0, 0.25, 0],[0, 0, 0, 0, 0.25]]
-data1 = np.random.multivariate_normal([0,0,0,0,0], mat1, 990)
+mat3=[[0.5, 0, 0, 0, 0],[0, 0.5, 0, 0, 0],[0, 0, 0.5, 0, 0],[0, 0, 0, 0.5, 0],[0, 0, 0, 0, 0.5]]
+data1 = np.random.multivariate_normal([0,0,0,0,0], mat1, 980)
 data2 = np.random.multivariate_normal([7.5,7.5,0,0,0], mat2, 10)
-dataset=np.concatenate((data1,data2),axis=0)
+data3 = np.random.multivariate_normal([0,0,0,5,5], mat3, 10)
+dataset=np.concatenate((data1,data2,data3),axis=0)
 
 # %%
 # Create model and load dataset for depth computation 
@@ -32,8 +34,8 @@ baseACA=model.ACA(dim=2,) # evaluate the dataset itself
 datasetACA=np.dot(dataset,baseACA)
 fig=plt.figure()
 plt.scatter(datasetACA[:,0],datasetACA[:,1], c="blue",label="Dataset")
-plt.xlabel("First component ACA")
-plt.ylabel("Second component ACA")
+plt.xlabel("First abnormal component (AC1)")
+plt.ylabel("Second abnormal component (AC2)")
 plt.title("Dataset visualization using abnormal component analysis")
 plt.legend()
 plt.show()
