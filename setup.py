268fef6731dccc81a75b796d0bc75ec298baea44
Leonardo Leone <leonardo.leone1998@gmail.com>
2026-04-02 10:29:28 +0200

    delete comment

--- setup.py
+++ setup.py
@@ -35,18 +35,15 @@ if sys.platform=='darwin' or sys.platform=='linux':
 		    "ddalpha", 
 		    sources=["depth/src/ddalpha.cpp"],
 		    extra_compile_args=["-I.",'-std=c++14','-fPIC','-O2'],
-      		# extra_compile_args=['-std=c++14','-fPIC','-O2'],
-		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
+      	    extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
 		),Extension(
 		    "depth_wrapper", 
 		    sources=["depth/src/depth_wrapper.cpp"],
-		    # extra_compile_args=["-I.",'-std=c++14','-fPIC','-O2'],
-      		extra_compile_args=['-std=c++14','-fPIC','-O2'],
+			extra_compile_args=['-std=c++14','-fPIC','-O2'],
 		    extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
 		),Extension(
             "ACA_wrapper",
             sources=["depth/src/ACA_wrapper.cpp"],
-            # extra_compile_args=['-std=c++14','-fPIC','-O2'],
             extra_compile_args=["-I.",'-std=c++14','-fPIC','-O2'],
             extra_link_args=["-rdynamic",'-std=c++14','-fPIC']
         )
